b34e1d6560d190e114b53bd2cf39d51e818fda5e
gabriela <gabriela.rojas.mt@usa.edu.co>
2022-10-11 11:55:54 -0600

    Primer commmit

---
+++ src/main/java/com/example/proyectoortopedic/ProyectoOrtopedicApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectoortopedic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoOrtopedicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoOrtopedicApplication.class, args);
+    }
+
+}
